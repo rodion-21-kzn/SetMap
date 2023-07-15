b6dab497de4934dd5c97dadf98c62f4e44864ae7
Oznak Banshee <oznakban@ge-b1.kzn.21-school.ru>
2023-07-15 17:40:18 +0300

    new tree with insert

--- AvlTree.cpp
+++ AvlTree.cpp
@@ -5,34 +5,98 @@
 #include "AvlTree.h"
 
 int main() {
-    std::cout << 2;
+    AVLTree new_tree;
+    int key = 1, value = 1;
+    for (int i = 0; i < 17; i++)
+        new_tree.Insert(key++,value++);
+    std::cout << 1;
 }
 
-Node::Node(int key, int value) {
-    key_ = key;
-    value_ = value;
+// Contructors
+
+AVLTree::AVLTree() : root_(nullptr) {}
+
+AVLTree::Node::Node(int key, int value) : key_(key), value_(value) {}
+
+// Support
+
+int AVLTree::GetHeight(AVLTree::Node *node) {
+    return node == nullptr ? -1 : node->height_;
 }
 
-int Node::GetHeight(Node* node) {
-    return node == nullptr ? -1:node->height_;
+int AVLTree::GetBalance(AVLTree::Node *node) {
+    return node == nullptr ? 0 : GetHeight(node->right_)  - GetHeight(node->left_);
 }
 
-void Node::UpdateHeight(Node *node) {
+void AVLTree::SetHeight(AVLTree::Node *node) {
     node->height_ = std::max(GetHeight(node->left_), GetHeight(node->right_)) + 1;
 }
 
-int Node::GetBalance(Node *node) {
-    return node == nullptr ? 0 : GetHeight(node->right_) - GetHeight(node->right_);
-}
-
-void Node::Swap(Node *a, Node *b) {
+void AVLTree::Swap(AVLTree::Node *a, AVLTree::Node *b) {
     std::swap(a->key_, b->key_);
     std::swap(a->value_, b->value_);
 }
 
-void Node::RightRotate(Node *node) {
+void AVLTree::RightRotate(AVLTree::Node *node) {
     Swap(node, node->left_);
-    Node temp = node->right_; // copy constructor or move;
-
-
+    Node* tmp = node->right_;
+    node->right_ = node->left_;
+    node->left_ = node->right_->left_;
+    node->right_->left_ = node->right_->right_;
+    node->right_->right_ = tmp;
+    SetHeight(node->right_);
+    SetHeight(node->left_);
 }
+
+void AVLTree::LeftRotate(AVLTree::Node *node) {
+    Swap(node, node->right_);
+    Node* tmp = node->left_;
+    node->left_ = node->right_;
+    node->right_ = node->left_->right_;
+//    node->right_->left_ = node->right_->right_; // похоже лишнее
+    node->left_->right_ =  node->left_->left_;
+    node->left_->left_ = tmp;
+    SetHeight(node->left_);
+    SetHeight(node);
+}
+
+void AVLTree::Balance(Node *node) {
+    int balance = GetBalance(node);
+    if (balance == -2) {
+        if(GetBalance(node->left_) == 1) LeftRotate(node->left_);
+        RightRotate(node);
+    } else if (balance == 2) {
+        if(GetBalance(node->right_) == -1) RightRotate(node->right_);
+        LeftRotate(node);
+    }
+}
+
+void AVLTree::Insert(int key, int value) {
+    if (root_ == nullptr) {
+        root_ = new Node(key, value);
+        tree_size_ = 1;
+    } else {
+        RecursiveInsert(root_, key, value);
+    }
+}
+
+void AVLTree::RecursiveInsert(AVLTree::Node *node, int key, int value) {
+    if (key < node->key_) {
+        if (node->left_ == nullptr) {
+            node->left_ = new Node(key,value);
+        } else {
+            RecursiveInsert(node->left_, key, value);
+        }
+    } else if (key > node->key_) {
+        if (node->right_ == nullptr) {
+            node->right_ = new Node(key,value);
+        } else {
+            RecursiveInsert(node->right_, key, value);
+        }
+    }
+    SetHeight(node);
+    Balance(node);
+}
+
+
+
--- AvlTree.h
+++ AvlTree.h
@@ -7,25 +7,39 @@
 
 #include <iostream>
 
-class Node {
+class AVLTree {
 
 public:
-    Node(int key, int value);
-    int GetHeight(Node* node);
-    int GetBalance(Node* node);
+    struct Node {
+        int key_;
+        int value_;
+        Node* left_ = nullptr;
+        Node* right_ = nullptr;
+        int height_ = 0;
 
+        Node(int key, int value);
+    };
+
+    AVLTree();
+    void Insert(int key, int value);
 
 private:
-    int key_;
-    int value_;
-    Node* left_;
-    Node* right_;
-    int height_;
+    Node* root_;
+    size_t tree_size_;
 
-    void UpdateHeight(Node* node);
+    // Support
+    int GetHeight(Node* node);
+    void SetHeight(Node* node);
+    int GetBalance(Node* node);
     void Swap(Node* a, Node* b);
-    void RightRotate(Node *node);
 
+    // Rotation
+    void RightRotate(Node* node);
+    void LeftRotate(Node* node);
+    void Balance(Node* node);
+
+    //
+    void RecursiveInsert(Node* node, int key, int value);
 };
 
 #endif //CONTAINERS_AVLTREE_H
